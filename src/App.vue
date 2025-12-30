83b7a2d3e409ca7d9891e9bf7e2ae75d62da326d
bkstory2 <bkstory2@gmail.com>
2025-12-30 17:42:42 +0900

    주석추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup></script>
 
 <template>
+  <!-- CALL-0009: 현재 라우트에 맞는 컴포넌트 렌더링 -->
+  <!-- CALL-0010: 라우터가 '/' 경로이므로 WordView.vue 표시 -->
   <WordView />
 </template>
 
 <script setup>
-  import WordView from "./views/WordView.vue";
+// CALL-0011: App 컴포넌트 설정 (간단한 경우 생략 가능)
+import WordView from './views/WordView.vue'
 </script>
